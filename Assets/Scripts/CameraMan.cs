7174a393e9321562050980691214b392946480c6
Domm Dynamite <DommDynamite@users.noreply.github.com>
2018-08-18 15:55:45 -0500

    Initial Upload
    I've done a little configuring but not much dev work. I have downloaded several useful assets including: VRTK, a Free 3D adventure character, and steam VR.

---
+++ Assets/Scripts/CameraMan.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMan : MonoBehaviour 
+{
+	Vector3 cameraPosition;
+
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+		
+	}
+
+}
