9266ff3f8e32d2c6cd884f88932cdf739077702c
Domm Dynamite <DommDynamite@users.noreply.github.com>
2018-09-10 20:55:34 -0500

    Added UFPS Asset Package

--- Assets/Scripts/FlameMechanics.cs
+++ Assets/Scripts/FlameMechanics.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
 public class FlameMechanics : MonoBehaviour 
 {
 	public GameObject FlameObject;
 	public float FlameVelocityLimit;
+	public VRTK_ControllerEvents ControllerEvents;
+	public bool FlameStatus;
 	Rigidbody playerHand;
 
 	void Start () 
 	{
-
+		//ControllerEvents.SubscribeToButtonAliasEvent(VRTK_ControllerEvents.ButtonAlias.TriggerClick, false, maintainFlame);
+		ControllerEvents.TriggerPressed += new ControllerInteractionEventHandler(getFlameStatus);
+		ControllerEvents.TriggerReleased += new ControllerInteractionEventHandler(maintainFlame);
 	}
 	
 	
@@ -30,4 +35,23 @@ public class FlameMechanics : MonoBehaviour
 			FlameObject.SetActive(true);
 		}
 	}
+
+	void getFlameStatus (object sender, ControllerInteractionEventArgs e)
+	{
+		Debug.Log("Flame Status Set. Button Pressure: " + e.buttonPressure);
+		if (FlameObject.activeSelf && e.buttonPressure <= .5f)
+		{
+			FlameStatus = true;
+		}
+	}
+
+	void maintainFlame(object sender, ControllerInteractionEventArgs e)
+	{
+		Debug.Log("Maintain Flame Attempted. Button Pressure: " + e.buttonPressure);
+		if (FlameStatus && e.buttonPressure <= .5f)
+		{
+			FlameObject.SetActive(true);
+		}
+	}
+
 }
---
+++ Assets/Scripts/Telekinesis.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Telekinesis : MonoBehaviour 
+{
+	GameObject grabbedObject;
+	public VRTK_Pointer Pointer;
+	public VRTK_ControllerEvents Controller;
+	public VRTK_StraightPointerRenderer Beam;
+	Vector2 thumbPosition;
+
+	void Start () 
+	{
+		Pointer.ActivationButtonPressed += new ControllerInteractionEventHandler(findObject);
+	}
+	
+	
+	void Update () 
+	{
+		if (grabbedObject != null)
+		{
+			try
+			{
+				thumbPosition = Controller.GetTouchpadAxis();
+				Debug.Log("Thumb Position: " + thumbPosition.x + ", " + thumbPosition.y);
+			}
+			catch (System.Exception)
+			{
+				Debug.Log("Couldn't retrieve thumb position.");
+			}
+			
+			if (thumbPosition.y > 0)
+			{
+				var Direction = (grabbedObject.transform.position - Beam.transform.parent.transform.position).normalized;
+				Vector3.MoveTowards(grabbedObject.transform.position, -Direction, 12f);
+				Debug.Log("Moving Forward");
+			}
+			if (thumbPosition.y < 0)
+			{
+				Vector3.MoveTowards(grabbedObject.transform.position, Beam.transform.parent.transform.position, 12f);
+				Debug.Log("Moving Backward");
+			}
+
+		}
+
+	}
+
+	void findObject (object sender, ControllerInteractionEventArgs e)
+	{
+		if (Beam.playareaCursor.validLocationObject == null)
+		{
+			return;
+		}
+		if (Beam.playareaCursor.validLocationObject != null)
+		{
+			grabbedObject = Beam.playareaCursor.validLocationObject;
+			Debug.Log("Object Grabbed: " + Beam.playareaCursor.validLocationObject.name);
+		}
+		else
+		{
+			grabbedObject = null;
+		}
+	}
+
+}
